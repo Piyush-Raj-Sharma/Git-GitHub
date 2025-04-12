8dcbf38573ea6b458e07d5dcaa51091c5a72c8c0
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-04-12 17:06:44 +0530

    this is my first commit while learning Git & Git Hub

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// git init - git starts managing whole file, its structure
+// git add . - adds each fil and folder to run
+// git commint -m "message" -> 
+
+// git add . & git commit -m are regularly used to create break point in your code  
+
+// git push
+
+const a = 10
